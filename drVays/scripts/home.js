07696a41530551f81982f275cc139cba1c061ccd
kattrine99 <kattren990703@gmail.com>
2026-05-10 14:59:37 +0500

    добавление дизайна хедера и бургер меню. такжедобавление шрифта и стлей

--- drVays/scripts/home.js
+++ drVays/scripts/home.js
@@ -1,8 +1,16 @@
-import { initializeHeader } from './header.js';
-import { initializeFooter } from './footer.js';
+const burger = document.getElementById("burger");
+const navigation = document.getElementById("navigation");
 
-document.addEventListener("DOMContentLoaded", async () => {
-    await includeHTML("[data-include]");
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+const mainWindow = document.getElementById("main-window");
 
-    initializeHeader();
-    initializeFooter();});
+scrollTopBtn.addEventListener("click", () => {
+  mainWindow.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+});
